b858eed6d4e111b5c62236acfcc4d85cd273dc4f
RedoFromStart <55228370+Redo-From-Start@users.noreply.github.com>
2020-05-24 15:35:19 +0100

    Create index.java

---
+++ java/index.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        
+        Scanner myObj = new Scanner(System.in);
+        myObj.nextLine();
+    }
+}
